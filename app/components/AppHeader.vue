eb29c1a9968bbcdc2be19ad50c2ea8eb13e46798
Egor Kutsiy <kycuj.egor2020@gmail.com>
2025-08-07 21:58:47 +0300

    feat: change header style

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -1,52 +1,74 @@
 <script setup lang="ts">
+import { NuxtLink } from "#components";
+
 const route = useRoute();
 </script>
 
 <template>
-  <nav class="background w-full h-18">
+  <nav class="background w-full h-[103px]">
     <div
-      class="container mx-auto px-28 h-full flex justify-between items-center"
+      class="container mx-auto px-[160px] h-full flex justify-between items-center"
     >
-      <div class="text-2xl font-medium">FutureTech</div>
-      <ul class="flex justify-between items-center gap-10">
-        <li
+      <NuxtLink
+        to="/"
+        class="text-2xl font-medium flex justify-center items-center gap-4 hover:cursor-pointer"
+      >
+        <img src="/Group.png" alt="" class="w-[50px] h-[50px]" />FutureTech
+      </NuxtLink>
+      <ul class="flex justify-between items-center gap-[40px]">
+        <NuxtLink
+          to="/"
           :class="
             route.path === '/'
               ? 'px-6 py-3 button_back border button_border rounded-[10px]'
-              : ''
+              : 'text-neutral-500'
           "
+          class="hover:cursor-pointer"
         >
           Home
-        </li>
-        <li
-          ::class="route.path === '/news' ? 'px-6 py-3 button_back border button_border rounded-[10px]' : ''"
+        </NuxtLink>
+        <NuxtLink
+          to="/news"
+          :class="
+            route.path === '/news'
+              ? ' px-6 py-3button_back border button_border rounded-[10px]'
+              : 'text-neutral-500'
+          "
+          class="hover:cursor-pointer"
         >
           News
-        </li>
-        <li
+        </NuxtLink>
+        <NuxtLink
+          to="/podcasts"
           :class="
             route.path === '/podcasts'
               ? 'px-6 py-3 button_back border button_border rounded-[10px]'
-              : ''
+              : 'text-neutral-500'
           "
+          class="hover:cursor-pointer"
         >
           Podcasts
-        </li>
-        <li
+        </NuxtLink>
+        <NuxtLink
+          to="/resources"
           :class="
             route.path === '/resources'
               ? 'px-6 py-3 button_back border button_border rounded-[10px]'
-              : ''
+              : 'text-neutral-500'
           "
+          class="hover:cursor-pointer"
         >
           Resources
-        </li>
+        </NuxtLink>
       </ul>
       <div class="flex justify-between items-center gap-10">
-        <div>Log In</div>
-        <div class="py-3 px-5 bg-amber-300 text-black rounded-[10px]">
+        <NuxtLink to="/log-in">Log In</NuxtLink>
+        <NuxtLink
+          to="/contacts"
+          class="py-3 px-5 bg-amber-300 text-black rounded-[10px]"
+        >
           Contact Us
-        </div>
+        </NuxtLink>
       </div>
     </div>
   </nav>
