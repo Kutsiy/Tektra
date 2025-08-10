ac7680b609aa5220b9ef2655684082fdaf970640
Egor Kutsiy <kycuj.egor2020@gmail.com>
2025-08-10 20:44:31 +0300

    feat: little changes

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,22 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="px-[var(--Indent)] bg-[var(--Dark-15)] w-full h-full"></div>
+  <div class="px-[var(--Indent)] bg-[var(--Dark-15)] w-full h-full py-[80px]">
+    <div class="text-[70px]">
+      <h1>Blog Platform</h1>
+    </div>
+    <div class="flex justify-between mt-[60px]">
+      <div class="w-[900px] h-full">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa, eos?
+      </div>
+      <div class="w-[250px] h-full">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, et
+        dolorum nam unde corporis sit inventore deleniti consequatur enim quia
+        doloribus esse dolore voluptatum, iure dolorem odio perspiciatis commodi
+        quis?Lorem1000
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
